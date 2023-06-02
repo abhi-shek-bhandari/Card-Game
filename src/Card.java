043f4ee7c150619d2e6a53d3c31b2703b9d7f571
abhi-shek-bhandari <abhishekbhandari06@outlook.com>
2023-06-02 15:43:12 +0530

    Card class created to Manage Rank and Suit enum

---
+++ src/Card.java
@@ -0,0 +1,29 @@
+public class Card {
+    private Suit suit;
+    private Rank rank;
+
+    public boolean isActionCard() {
+        return rank == Rank.ACE || rank == Rank.KING || rank == Rank.QUEEN || rank == Rank.JACK;
+    }
+
+    public Suit getSuit() {
+        return suit;
+    }
+
+    public void setSuit(Suit suit) {
+        this.suit = suit;
+    }
+
+    public Rank getRank() {
+        return rank;
+    }
+
+    public void setRank(Rank rank) {
+        this.rank = rank;
+    }
+
+    public Card(Suit suit, Rank rank) {
+        this.suit = suit;
+        this.rank = rank;
+    }
+}
